7bcffd4537c455c033e6c8649dbd4864864d5dc5
Atraxi <jdog-jdog@hotmail.com>
2015-09-13 04:16:34 +1200

    Beginnings of rewrite, incomplete and not used at all

--- src/atraxi/game/World.java
+++ src/atraxi/game/World.java
@@ -1,9 +1,15 @@
 package atraxi.game;
 
-import java.awt.Rectangle;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+import atraxi.game.UI.UIElement;
+import atraxi.game.UI.UIStack;
+import atraxi.game.UI.UIStackNode;
+import atraxi.game.UI.UserInterfaceHandler;
 import entities.Entity;
 import entities.Ship;
 import entities.Structure;
@@ -12,7 +18,9 @@ public class World
 {
     private final static ArrayList<Entity> entities = new ArrayList<Entity>();
     public static final int WORLDWIDTH = 1920;
-    public static final int WORLDHIEGHT = 1080;
+    public static final int WORLDHEIGHT = 1080;
+    private static final int GRIDWIDTH = 20;
+    private static final int GRIDHEIGHT = 20;
     
     public World()
     {
@@ -25,6 +33,8 @@ public class World
         entities.add(new Ship("baseShipClass", Game.getPlayerList().get(0), 100, 320));
         
         entities.add(new Structure("baseBuildingClass", Game.getPlayerList().get(0), 500, 500));
+
+        UserInterfaceHandler.uiStack.push(new Grid(new Rectangle(0,0,GRIDWIDTH,GRIDHEIGHT)));
     }
     
     /**
@@ -63,6 +73,125 @@ public class World
         synchronized(entities)
         {
             entities.add(entity);
+        }
+    }
+
+    public class Grid extends UIStack implements UIStackNode
+    {
+        private UIStackNode nextNode = null, previousNode = null;
+        private GridTile[][] tiles;
+
+        public Grid(Rectangle bounds)
+        {
+            tiles = new GridTile[WORLDWIDTH/bounds.width][WORLDHEIGHT/bounds.height];
+            for(int x=0;x<tiles.length;x++)
+            {
+                for(int y=0;y<tiles[x].length;y++)
+                {
+                    tiles[x][y] = new GridTile();
+                }
+            }
+        }
+
+        @Override
+        public UIStackNode getNextNode ()
+        {
+            return nextNode;
+        }
+
+        @Override
+        public UIStackNode getPreviousNode ()
+        {
+            return previousNode;
+        }
+
+        @Override
+        public void setNextNode (UIStackNode node)
+        {
+            nextNode = node;
+        }
+
+        @Override
+        public void setPreviousNode (UIStackNode node)
+        {
+            previousNode = node;
+        }
+
+        private class GridTile implements UIElement, UIStackNode
+        {
+            private UIStackNode nextNode = null, previousNode = null;
+
+            @Override
+            public UIStackNode getNextNode ()
+            {
+                return nextNode;
+            }
+
+            @Override
+            public UIStackNode getPreviousNode ()
+            {
+                return previousNode;
+            }
+
+            @Override
+            public void setNextNode (UIStackNode node)
+            {
+                nextNode = node;
+            }
+
+            @Override
+            public void setPreviousNode (UIStackNode node)
+            {
+                previousNode = node;
+            }
+
+            @Override
+            public boolean mousePressed (MouseEvent paramMouseEvent)
+            {
+                return false;
+            }
+
+            @Override
+            public boolean mouseReleased (MouseEvent paramMouseEvent)
+            {
+                return false;
+            }
+
+            @Override
+            public boolean mouseEntered (MouseEvent paramMouseEvent)
+            {
+                return false;
+            }
+
+            @Override
+            public boolean mouseExited (MouseEvent paramMouseEvent)
+            {
+                return false;
+            }
+
+            @Override
+            public boolean mouseDragged (MouseEvent paramMouseEvent)
+            {
+                return false;
+            }
+
+            @Override
+            public boolean mouseMoved (MouseEvent paramMouseEvent)
+            {
+                return false;
+            }
+
+            @Override
+            public boolean mouseWheelMoved (MouseWheelEvent paramMouseWheelEvent)
+            {
+                return false;
+            }
+
+            @Override
+            public void paint (Graphics2D graphics)
+            {
+
+            }
         }
     }
 }
