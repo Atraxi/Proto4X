549c07eb43f6214c082a8b8b8596e7a29e0b1d5c
Jason Hiatt <jason.w.hiatt@gmail.com>
2016-06-27 01:50:08 +1200

    minor documentation, optimisation, and refactoring. Also cleaning up some GridTile references in comments and variable names

--- Client/src/atraxi/client/ui/wrappers/WorldUIWrapper.java
+++ Client/src/atraxi/client/ui/wrappers/WorldUIWrapper.java
@@ -30,12 +30,6 @@ public class WorldUIWrapper extends World
      */
     private static final double gradient = rise / run;
 
-    //Tiles cannot be relied upon to update their state internally in all cases (especially reverting state), as it is not feasible to have every tile process every UI event
-    // (which includes mouse movement)
-    //We can mathematically determine newly focused tiles, and thus it can reliably set it's own state, but not implicitly know when to revert to default (as there is no 'out of
-    // focus' event)
-    //So we track tiles with special state and prompt them to check the conditions for reverting to default when relevant
-    //This is effectively registering tiles to always receive specific events
     private Point tileSelected, tileHover, tilePressed;
 
     private Globals.Identifiers imageDefault, imageHover, imageClick, imageSelected;
@@ -95,37 +89,23 @@ public class WorldUIWrapper extends World
 
     public static Point getGridTileIndexFromPixelLocation(int mouseX, int mouseY, WorldUIWrapper world)
     {
-        int x, y;
-        //start with a re-arranged version of the code from GridTile constructor
-        //used here to convert pixel location back to GridTile index
-        //not a perfect solution yet due to working with rectangle bounding box containing a hexagon
+        //start with a re-arranged version of the equation from Get{X,Y}Coord()
+        //used here to convert pixel location back to a tile/Entity index
+        //  not a perfect solution initially due to working with rectangle bounding box containing a hexagon
 
         //a useful estimate however with inaccuracies between top left/right corners of
-        //hex/rect bounds (bottom of hex/rect is the next row by this approximation)
-        y = (int) Math.floor(mouseY / ((3.0 * (double) world.getTileHeight()) / 4.0));
-        x = (int) Math.floor((mouseX - (y * (double) world.getTileWidth()) / 2.0) / (double) world.getTileWidth());
+        //  hex/rect bounds (top of hex/rect is the previous row by this approximation)
+        int y = (int) Math.floor(mouseY / ((3 * (double) world.getTileHeight()) / 4));
+        int x = (int) Math.floor((mouseX - (y * (double) world.getTileWidth()) / 2) / world.getTileWidth());
 
         //use the estimate to offset our coordinates to [0,0]
-        //in order to simplify math with gradients to separate overlapping hex bounds
-        int translatedX;
-        int translatedY;
-        //We just re-use the calculation from the GridTile constructor
-        //This is also the original calculation for the re-arranged version a few lines above
-        //The re-use here is basically equivalent to rounding, to get the top-left corner of the GridTile the mouse is currently over
-        //--------------------------------------------------------------
-        //horizontally is the same as rectangles
-        int xCoord = x * world.getTileWidth()
-                     //except offset by half the width of a hex every second row
-                     //if xIndex is even, then add 0, else add dimBounds.width/2
-                     + (y * world.getTileWidth() / 2);
-        int yCoord = y * (3 * world.getTileHeight() / 4);
-        //--------------------------------------------------------------
-        translatedX = mouseX - xCoord;
-        translatedY = mouseY - yCoord;
+        //  in order to simplify math with gradients to separate overlapping hex bounds
+        int translatedX = mouseX - getXCoord(x, y, world);
+        int translatedY = mouseY - getYCoord(y, world);
 
         //using pre-calculated constants for y=mx+c equation
-        //determine which corner we are in, if any, and offset index
-        //as needed to compensate for above mentioned inaccuracies with initial approximation
+        //  determine which corner we are in, if any, and offset index
+        //  as needed to compensate for above mentioned inaccuracies with initial approximation
         if(translatedY < (-gradient * translatedX + rise))
         {//cursor is actually in the top right corner of the rectangle
             //move up left
@@ -145,7 +125,7 @@ public class WorldUIWrapper extends World
         //index of the tile
         Point gridIndex = getGridTileIndexFromPixelLocation(paramMouseEvent.getX(), paramMouseEvent.getY(), this);
         //avoid index out of bounds errors
-        if(isWithinPlayableArea(gridIndex.x, gridIndex.y))
+        if(isWithinPlayableArea(gridIndex))
         {
             tilePressed = gridIndex;
             if(tileHover.equals(tilePressed))
@@ -165,7 +145,7 @@ public class WorldUIWrapper extends World
         Point gridIndex = getGridTileIndexFromPixelLocation(paramMouseEvent.getX(), paramMouseEvent.getY(), this);
 
         //avoid index out of bounds errors
-        if (isWithinPlayableArea(gridIndex.x, gridIndex.y))
+        if (isWithinPlayableArea(gridIndex))
         {
             //if the mousePressed() event was within the playable area. if not the linked mouseReleased() is irrelevant
             if(tilePressed.x >= 0 && tilePressed.y >= 0)
@@ -189,7 +169,7 @@ public class WorldUIWrapper extends World
 //        //index of the tile
 //        Point gridIndex = getGridTileIndexFromPixelLocation(paramMouseEvent.getX(), paramMouseEvent.getY(), this);
 //        //avoid index out of bounds errors
-//        if(isWithinPlayableArea(gridIndex.x, gridIndex.y))
+//        if(isWithinPlayableArea(gridIndex))
 //        {
 //            tileHover = gridIndex;
 //
@@ -207,7 +187,7 @@ public class WorldUIWrapper extends World
         //index of the tile
         Point gridIndex = getGridTileIndexFromPixelLocation(paramMouseEvent.getX(), paramMouseEvent.getY(), this);
         //avoid index out of bounds errors
-        if(isWithinPlayableArea(gridIndex.x, gridIndex.y))
+        if(isWithinPlayableArea(gridIndex))
         {
             tileHover = gridIndex;
 
@@ -224,7 +204,7 @@ public class WorldUIWrapper extends World
 //        //index of the tile
 //        Point gridIndex = getGridTileIndexFromPixelLocation(paramMouseWheelEvent.getX(), paramMouseWheelEvent.getY(), this);
 //        //avoid index out of bounds errors
-//        if(isWithinPlayableArea(gridIndex.x, gridIndex.y))
+//        if(isWithinPlayableArea(gridIndex))
 //        {
 //            //do something
 //        }
@@ -235,9 +215,9 @@ public class WorldUIWrapper extends World
         return null;
     }
 
-    private boolean isWithinPlayableArea(int x, int y)
+    private boolean isWithinPlayableArea(Point point)
     {
-        Point offset = convertAxialToOffset(x, y);
+        Point offset = convertAxialToOffset(point);
         return offset.x >= 0 && offset.y >= 0 && offset.x < getSizeX() && offset.y < getSizeY();
     }
 
@@ -292,13 +272,14 @@ public class WorldUIWrapper extends World
         //Axial co-ordinate system
         //horizontally is the same as rectangles
         return xIndex * world.getTileWidth()
-                        //except y-axis is at a 30degree angle, offsetting (rectangular screen) x co-ordinate
-                        //xIndex is offset by yIndex * dimBounds.width/2
+                        //except y-axis is at a 30degree angle, offsetting the X co-ordinate
+                        //xIndex is offset by yIndex * half the width of a tile
                         + (yIndex * world.getTileWidth() / 2);
     }
 
     public static int getYCoord(int yIndex, WorldUIWrapper world)
     {
+        //Hexagonal grids tile with a spacing of 3/4 the height of a hexagon, otherwise the same as rectangles
         return yIndex * (3 * world.getTileHeight() / 4);
     }
 
